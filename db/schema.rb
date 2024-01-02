0c7fd579dbb7c2c4c2dceffce2edddd60313268a
neeraj5511 <malhotraneerajneeraj30@gmail.com>
2024-01-02 13:26:31 +0530

    swtich from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_12_01_161453) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "students", force: :cascade do |t|
     t.integer "student_id"
     t.integer "s1"
